6c3317458ef6dba72d0200adaf3fcc9f708fe849
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2017-06-24 18:19:18 +0530

    Remove wrong usage of local.

--- roles/repo_updater/files/check_updates.sh
+++ roles/repo_updater/files/check_updates.sh
@@ -7,12 +7,12 @@ SSH_PRE="ssh -i ~/.ssh/id_rsa-"
 for repo in $BASE/*; do
     cd $repo
 
-    local e=${SSH_PRE}$(basename $repo)
+    e=${SSH_PRE}$(basename $repo)
 
     GIT_SSH_COMMAND="$e" git fetch
 
-    local head=$(GIT_SSH_COMMAND="$e" git rev-parse HEAD)
-    local remote=$(GIT_SSH_COMMAND="$e" git rev-parse @{u})
+    head=$(GIT_SSH_COMMAND="$e" git rev-parse HEAD)
+    remote=$(GIT_SSH_COMMAND="$e" git rev-parse @{u})
     if [ $head != $remote ]; then
 	echo $repo
     fi
